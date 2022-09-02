4dd934d24522352c31253e33758aef638ea699fb
Gulshan Dubey <gulshan.dubey@krishnacollege.ac.in>
2022-09-02 19:00:00 +0530

    theme_changer(Nagarro)

---
+++ app.js
@@ -0,0 +1,26 @@
+function black() {
+  document.getElementById("outer").style.backgroundColor = "black";
+  document.getElementById("text").style.borderColor = "white";
+  document.getElementById("text").style.backgroundColor = "black";
+  document.getElementById("text").style.color = "white";
+}
+
+function yellow() {
+  document.getElementById("outer").style.backgroundColor = "yellow";
+  document.getElementById("text").style.borderColor = "black";
+  document.getElementById("text").style.backgroundColor = "yellow";
+  document.getElementById("text").style.color = "black";
+}
+
+function blue() {
+  document.getElementById("outer").style.backgroundColor = "blue";
+  document.getElementById("text").style.borderColor = "white";
+  document.getElementById("text").style.backgroundColor = "blue";
+  document.getElementById("text").style.color = "white";
+}
+function green() {
+  document.getElementById("outer").style.backgroundColor = "green";
+  document.getElementById("text").style.borderColor = "black";
+  document.getElementById("text").style.backgroundColor = "green";
+  document.getElementById("text").style.color = "black";
+}
